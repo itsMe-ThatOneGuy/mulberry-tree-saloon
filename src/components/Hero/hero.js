f37db6a0ff49b870c617c65b6b85ddc738fdc09e
Matthew Smart <matthewsmart13@gmail.com>
2023-05-08 14:15:04 -0500

    Basic hero section layout

---
+++ src/components/Hero/hero.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const Hero = () => {
+	return (
+		<section>
+			<div className="container">
+				<div>
+					<h1>Mulberry Tree</h1>
+					<h1>Saloon</h1>
+				</div>
+			</div>
+		</section>
+	);
+};
