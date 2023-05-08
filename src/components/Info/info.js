f3d77e5496a18af5deed2b84b895601632265114
Matthew Smart <matthewsmart13@gmail.com>
2023-05-08 16:21:51 -0500

    Info section basic layout

---
+++ src/components/Info/info.js
@@ -0,0 +1,22 @@
+const Info = () => {
+	return (
+		<section id="info">
+			<div>
+				<h2>Information</h2>
+			</div>
+
+			<div>
+				{/* Google maps image*/}
+				<div></div>
+
+				{/* contact info */}
+				<div></div>
+
+				{/* hours */}
+				<div></div>
+			</div>
+		</section>
+	);
+};
+
+export default Info;
