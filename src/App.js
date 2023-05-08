56532f8271d330a206829a486f129b08f1209a69
Matthew Smart <matthewsmart13@gmail.com>
2023-05-08 16:22:39 -0500

    Added info component to page layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header/header";
 import Hero from "./components/Hero/hero";
 import Footer from "./components/Footer/footer";
+import Info from "./components/Info/info";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Hero />
+			<Info />
 			<Footer />
 		</div>
 	);
