5c2f5c485cbc6ae9d94c620709578015483306f6
Matthew Smart <matthewsmart13@gmail.com>
2023-05-08 15:56:21 -0500

    Basic footer layout

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,28 @@
+import { Facebook } from "react-feather";
+
+const Footer = () => {
+	return (
+		<footer>
+			<div className="logo">MTS</div>
+
+			<div className="text">
+				<p>
+					© {new Date().getFullYear()} &middot; Mulberry Tree Saloon.
+					All rights reserved.
+				</p>
+			</div>
+
+			<div className="socials">
+				<a
+					href="https://www.facebook.com/profile.php?id=100057060955346"
+					target="_blank">
+					<button>
+						<Facebook />
+					</button>
+				</a>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
