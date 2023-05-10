fc96315feed84d28808bb378aa42380bad8103f8
Matthew Smart <matthewsmart13@gmail.com>
2023-05-10 12:35:43 -0500

    Added styling

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,12 +1,16 @@
-import { useState } from "react";
-
 const Hero = () => {
 	return (
-		<section>
+		<section className="relative flex align-center h-screen">
 			<div className="container">
-				<div>
-					<h1>Mulberry Tree</h1>
-					<h1>Saloon</h1>
+				<div className="bg-fixed bg-cover bg-center absolute top-0 left-0 w-full h-full">
+					<div className="h-screen flex flex-col justify-center items-center">
+						<h1 className="z-[5] pt-12 text-2xl md:text-3xl text-[#222] font-normal uppercase tracking-[.2em] drop-shadow">
+							Mulberry Tree
+						</h1>
+						<h1 className="z-[5] pt-12 text-2xl md:text-3xl text-[#222] font-normal uppercase tracking-[.2em] drop-shadow">
+							Saloon
+						</h1>
+					</div>
 				</div>
 			</div>
 		</section>
