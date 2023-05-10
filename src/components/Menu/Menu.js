d50f3b8d4ee43cf1efaa7538f73a40dbbb719e90
Matthew Smart <matthewsmart13@gmail.com>
2023-05-10 01:22:42 -0500

    Created basic menu component

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,16 @@
+import MenuItem from "./MenuItem";
+
+const Menu = () => {
+	return (
+		<section>
+			<div>
+				<h2>Menu</h2>
+			</div>
+			<div>
+				<MenuItem />
+			</div>
+		</section>
+	);
+};
+
+export default Menu;
