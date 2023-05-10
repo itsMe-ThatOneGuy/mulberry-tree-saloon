0bddee20f1ea148bae184c7cbae01d989bd1d08d
Matthew Smart <matthewsmart13@gmail.com>
2023-05-10 01:22:10 -0500

    Added properties to fix bug

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -11,6 +11,7 @@ const Info = () => {
 				{/* Google maps image*/}
 				<div>
 					<iframe
+						title="test"
 						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6373.141844684716!2d-93.63045578181291!3d36.99617396565907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87cf4eda7f665649%3A0xbeb42b2f2611bf8e!2s610%20State%20Hwy%20265%2C%20Marionville%2C%20MO%2065705!5e0!3m2!1sen!2sus!4v1683601716692!5m2!1sen!2sus"
 						width="600"
 						height="450"
@@ -103,7 +104,8 @@ const Info = () => {
 				<div>
 					<a
 						href="https://www.facebook.com/profile.php?id=100057060955346"
-						target="_blank">
+						target="_blank"
+						rel="noreferrer">
 						<button>
 							<Facebook />
 						</button>
