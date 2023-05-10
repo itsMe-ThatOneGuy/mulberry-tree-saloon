3c5d16ad1e5ab774b5ca1c266dc6dd7c488fa61f
Matthew Smart <matthewsmart13@gmail.com>
2023-05-10 01:19:17 -0500

    Added Menu and About

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Header from "./components/Header/header";
 import Hero from "./components/Hero/hero";
 import Footer from "./components/Footer/footer";
 import Info from "./components/Info/info";
+import Menu from "./components/Menu/Menu";
+import About from "./components/About/About";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Hero />
+			<About />
+			<Menu />
 			<Info />
 			<Footer />
 		</div>
