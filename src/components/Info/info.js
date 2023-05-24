4b98e94576cc2ef8e47c6f5fb10eda4b876740db
Matthew Smart <matthewsmart13@gmail.com>
2023-05-23 22:41:01 -0500

    Fixed typos in iframe props

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -13,9 +13,9 @@ const Info = () => {
 							<iframe
 								title="test"
 								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6373.141844684716!2d-93.63045578181291!3d36.99617396565907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87cf4eda7f665649%3A0xbeb42b2f2611bf8e!2s610%20State%20Hwy%20265%2C%20Marionville%2C%20MO%2065705!5e0!3m2!1sen!2sus!4v1683601716692!5m2!1sen!2sus"
-								allowfullscreen=""
+								allowFullScreen=""
 								loading="lazy"
-								referrerpolicy="no-referrer-when-downgrade"
+								referrerPolicy="no-referrer-when-downgrade"
 								className="mr-4 drop-shadow-md md:w-[544px] md:h-[492px]"></iframe>
 						</div>
 
