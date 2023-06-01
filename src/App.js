c94e9c624c758a29f5a97f05f56fc3c400d09541
Matthew Smart <matthewsmart13@gmail.com>
2023-06-01 15:08:08 -0500

    removed unused style import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Header from "./components/Header/header";
 import Hero from "./components/Hero/hero";
 import Footer from "./components/Footer/footer";
@@ -7,16 +6,16 @@ import Menu from "./components/Menu/Menu";
 import About from "./components/About/About";
 
 function App() {
-	return (
-		<div className="App">
-			<Header />
-			<Hero />
-			<About />
-			<Menu />
-			<Info />
-			<Footer />
-		</div>
-	);
+    return (
+        <div className="App">
+            <Header />
+            <Hero />
+            <About />
+            <Menu />
+            <Info />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
