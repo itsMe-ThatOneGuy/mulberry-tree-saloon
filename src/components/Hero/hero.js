8b6d08df6d82b97c292a73f1ca71f6df8636cd7a
Matthew Smart <matthewsmart13@gmail.com>
2023-06-04 17:07:02 -0500

    Finished mobile Header

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -5,14 +5,14 @@ const Hero = () => {
 		<section className="relative flex items-center h-screen z-30">
 			<div>
 				<div
-					className="bg-fixed bg-contain md:cover bg-center absolute top-0 left-0 w-full h-full"
+					className="bg-fixed bg-contain md:bg-cover bg-center absolute top-0 left-0 w-full h-full"
 					style={{ backgroundImage: `url("${heroImage}")` }}
 				>
 					<div className="bg-black bg-opacity-60 h-screen flex flex-col justify-center items-center">
-						<h1 className="text-white text-2xl font-bold uppercase tracking-[.2em] drop-shadow">
-							Mulberry Tree
+						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] drop-shadow">
+							Mulberry <span className="text-green-500">Tree</span>
 						</h1>
-						<h1 className="text-white text-2xl font-bold uppercase tracking-[.2em] edrop-shadow pt-12">
+						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] edrop-shadow pt-12">
 							Saloon
 						</h1>
 					</div>
