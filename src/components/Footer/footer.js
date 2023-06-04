76b733205d0a6ce0a42a32ec9d5e50c4455709ab
Matthew Smart <matthewsmart13@gmail.com>
2023-06-03 21:49:14 -0500

    Finished mobile footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,24 +1,23 @@
-import { Facebook } from "react-feather";
+import { Facebook } from 'react-feather';
 
 const Footer = () => {
 	return (
 		<footer>
-			<div>
-				<div>MTS</div>
+			<div className="relative bg-green-500 w-full flex justify-between py-3 text-white items-center">
+				<div className="ml-7">MTS</div>
 
-				<div>
-					<p>
-						© {new Date().getFullYear()} &middot; Mulberry Tree
-						Saloon. All rights reserved.
-					</p>
+				<div className="flex flex-col items-center">
+					<p>© {new Date().getFullYear()} &middot; Mulberry Tree Saloon.</p>
+					<p>All rights reserved.</p>
 				</div>
 
 				<div>
 					<a
 						href="https://www.facebook.com/profile.php?id=100057060955346"
 						target="_blank"
-						rel="noreferrer">
-						<button>
+						rel="noreferrer"
+					>
+						<button className="mr-7">
 							<Facebook size={20} />
 						</button>
 					</a>
