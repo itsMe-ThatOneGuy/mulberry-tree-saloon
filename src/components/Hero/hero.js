9687f73aff35b826860e3ea560e7416858918593
Matthew Smart <matthewsmart13@gmail.com>
2023-06-03 19:38:20 -0500

    Finished mobile hero section

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,11 +1,20 @@
+import heroImage from '../../images/heroImage.jpg';
+
 const Hero = () => {
 	return (
-		<section>
+		<section className="relative flex items-center h-screen z-30">
 			<div>
-				<div>
-					<div>
-						<h1>Mulberry Tree</h1>
-						<h1>Saloon</h1>
+				<div
+					className="bg-fixed bg-contain md:cover bg-center absolute top-0 left-0 w-full h-full"
+					style={{ backgroundImage: `url("${heroImage}")` }}
+				>
+					<div className="bg-black bg-opacity-60 h-screen flex flex-col justify-center items-center">
+						<h1 className="text-white text-2xl font-bold uppercase tracking-[.2em] drop-shadow">
+							Mulberry Tree
+						</h1>
+						<h1 className="text-white text-2xl font-bold uppercase tracking-[.2em] edrop-shadow pt-12">
+							Saloon
+						</h1>
 					</div>
 				</div>
 			</div>
