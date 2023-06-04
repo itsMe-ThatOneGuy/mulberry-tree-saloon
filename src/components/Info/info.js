23d83cd6b2f42d4eef774e00ee074229b5493c24
Matthew Smart <matthewsmart13@gmail.com>
2023-06-04 17:07:30 -0500

    Finished mobile Info comp

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -1,111 +1,111 @@
-import { MapPin, Mail, Clock, Phone, Facebook } from "react-feather";
+import { MapPin, Mail, Clock, Phone, Facebook } from 'react-feather';
 
 const Info = () => {
 	return (
-		<section id="info">
-			<div>
-				<div>
-					<h2>Information</h2>
+		<section id="info" className="relative">
+			<div className="my-5">
+				<div className="bg-[#222] flex flex-col items-center p-3">
+					<h2 className="pb-8 mt-7 font-medium text-green-500 text-2xl text-center uppercase tracking-widest">
+						- Information -
+					</h2>
 
-					<div>
+					<div className="text-white w-fllull flex flex-col items-center">
 						{/* Google maps image*/}
-						<div>
+						<div className="w-3/4">
 							<iframe
+								className="w-full h-72"
 								title="test"
 								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6373.141844684716!2d-93.63045578181291!3d36.99617396565907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87cf4eda7f665649%3A0xbeb42b2f2611bf8e!2s610%20State%20Hwy%20265%2C%20Marionville%2C%20MO%2065705!5e0!3m2!1sen!2sus!4v1683601716692!5m2!1sen!2sus"
 								allowfullscreen=""
 								loading="lazy"
-								referrerpolicy="no-referrer-when-downgrade"></iframe>
+								referrerpolicy="no-referrer-when-downgrade"
+							></iframe>
 						</div>
 
 						{/* contact info */}
-						<div>
-							<div>
-								<MapPin size={20} />
-								<p>Location :</p>
-							</div>
-							<div>
-								<p>
-									610 State Hwy 265
-									<br />
-									Marionville, MO 65705
-								</p>
-							</div>
-
-							<div>
-								<Phone size={20} />
-								<p>Phone :</p>
-							</div>
-							<div>
-								<p>(417) 366-9857</p>
+						<div className="py-7 font-semibold text-lg">
+							<div className="flex flex-col items-center py-3">
+								<div className="flex pb-3">
+									<MapPin className="text-green-500" size={20} />
+									<p className="pl-2">Location :</p>
+								</div>
+								<div>
+									<p>
+										610 State Hwy 265
+										<br />
+										Marionville, MO 65705
+									</p>
+								</div>
 							</div>
 
-							<div>
-								<Mail size={20} />
-								<p>Email :</p>
-							</div>
-							<div>
-								<p>MulberryMail@yahoo.com</p>
+							<div className="flex flex-col items-center py-3">
+								<div className="flex pb-3">
+									<Phone className="text-green-500" size={20} />
+									<p className="pl-2">Phone :</p>
+								</div>
+								<div>
+									<p>(417) 366-9857</p>
+								</div>
 							</div>
 
-							<div>
-								<Clock size={20} />
-								<p>Hours of Operation :</p>
+							<div className="flex flex-col items-center py-3">
+								<div className="flex pb-3">
+									<Mail className="text-green-500" size={20} />
+									<p className="pl-2">Email :</p>
+								</div>
+								<div>
+									<p>MulberryMail@yahoo.com</p>
+								</div>
 							</div>
-							<div></div>
-							<div>
-								<p>Sun</p>
-							</div>
-							<div>
-								<p>12:00 PM - 8:00 PM</p>
-							</div>
-							<div>
-								<p>Mon</p>
-							</div>
-							<div>
-								<p>Closed</p>
-							</div>
-							<div>
-								<p>Tues</p>
-							</div>
-							<div>
-								<p>4:00 PM - 12:00 AM</p>
-							</div>
-							<div>
-								<p>Wed</p>
-							</div>
-							<div>
-								<p>4:00 PM - 12:00 AM</p>
-							</div>
-							<div>
-								<p>Thurs</p>
-							</div>
-							<div>
-								<p>4:00 PM - 12:00 AM</p>
-							</div>
-							<div>
-								<p>Fri</p>
-							</div>
-							<div>
-								<p>4:00 PM - 12:00 AM</p>
-							</div>
-							<div>
-								<p>Sat</p>
-							</div>
-							<div>
-								<p>4:00 PM - 12:00 AM</p>
+
+							<div className="flex flex-col items-center py-3">
+								<div className="flex pb-3">
+									<Clock className="text-green-500" size={20} />
+									<p className="pl-2">Hours of Operation :</p>
+								</div>
+								<div className="flex flex-col w-screen px-28">
+									<div className="flex justify-between pb-1">
+										<p className="text-green-500">Sun</p>
+										<p>12:00 PM - 8:00 PM</p>
+									</div>
+									<div className="flex justify-between pb-1">
+										<p>Mon</p>
+										<p>Closed</p>
+									</div>
+									<div className="flex justify-between pb-1">
+										<p>Tues</p>
+										<p>4:00 PM - 12:00 AM</p>
+									</div>
+									<div className="flex justify-between pb-1">
+										<p>Wed</p>
+										<p>4:00 PM - 12:00 AM</p>
+									</div>
+									<div className="flex justify-between pb-1">
+										<p>Thurs</p>
+										<p>4:00 PM - 12:00 AM</p>
+									</div>
+									<div className="flex justify-between pb-1">
+										<p>Fri</p>
+										<p>4:00 PM - 12:00 AM</p>
+									</div>
+									<div className="flex justify-between pb-1">
+										<p className="text-green-500">Sat</p>
+										<p>4:00 PM - 12:00 AM</p>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
-					<div>
+					<div className="text-white font-medium text-xl py-3">
 						<p>Visit our Facebook page for all upcoming Events!</p>
 					</div>
-					<div>
+					<div className="py-5">
 						<a
 							href="https://www.facebook.com/profile.php?id=100057060955346"
 							target="_blank"
-							rel="noreferrer">
-							<button>
+							rel="noreferrer"
+						>
+							<button className="text-white hover:bg-blue-500 ease-in-out duration-75 rounded-3xl p-3">
 								<Facebook size={30} />
 							</button>
 						</a>
