8373342e877cdfc22a187e9c48469fc63e813776
Matthew Smart <matthewsmart13@gmail.com>
2023-06-05 10:31:08 -0500

    Finished desktop About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,15 @@
 const About = () => {
 	return (
-		<section id="about" className="relative w-full">
+		<section id="about" className="relative w-full h-full">
 			<div className="my-5">
-				<div className="bg-[#222]">
-					<div className="flex flex-col items-center py-10 px-10">
-						<h2 className="pb-8 font-medium text-green-500 text-2xl text-center uppercase tracking-widest">
+				<div className="bg-[#222] py-3">
+					<div className="flex flex-col items-center py-10 px-10 md:mx-80">
+						<h2 className="pb-10 font-medium text-green-500 text-2xl text-center uppercase tracking-widest md:text-5xl">
 							- Our Story -
 						</h2>
 
-						<div className="text-white">
-							<p className="pb-2">
+						<div className="text-white text-center md:text-2xl font-normal">
+							<p className="pb-3">
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
 								enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -18,7 +18,7 @@ const About = () => {
 								nulla pariatur. Excepteur sint occaecat cupidatat non proident,
 								sunt in culpa qui officia deserunt mollit anim id est laborum.
 							</p>
-							<p className="pb-2">
+							<p className="pb-3">
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
 								enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -27,7 +27,7 @@ const About = () => {
 								nulla pariatur. Excepteur sint occaecat cupidatat non proident,
 								sunt in culpa qui officia deserunt mollit anim id est laborum.
 							</p>
-							<p className="pb-2">
+							<p className="pb-3">
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
 								enim ad minim veniam, quis nostrud exercitation ullamco laboris
