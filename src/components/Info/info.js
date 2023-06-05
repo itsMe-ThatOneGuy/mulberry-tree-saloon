5080e618b323c53921fb07e685e31d593ee14014
Matthew Smart <matthewsmart13@gmail.com>
2023-06-05 16:17:04 -0500

    Fixed typo in iframe

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -16,9 +16,8 @@ const Info = () => {
 									className="w-full h-72 md:h-[30rem]"
 									title="test"
 									src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6373.141844684716!2d-93.63045578181291!3d36.99617396565907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87cf4eda7f665649%3A0xbeb42b2f2611bf8e!2s610%20State%20Hwy%20265%2C%20Marionville%2C%20MO%2065705!5e0!3m2!1sen!2sus!4v1683601716692!5m2!1sen!2sus"
-									allowfullscreen=""
 									loading="lazy"
-									referrerpolicy="no-referrer-when-downgrade"
+									referrerPolicy="no-referrer-when-downgrade"
 								></iframe>
 							</div>
 						</div>
