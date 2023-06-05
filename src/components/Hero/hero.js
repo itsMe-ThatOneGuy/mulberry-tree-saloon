349829f87ef5ef8587924863f4d4d17f8acac4ea
Matthew Smart <matthewsmart13@gmail.com>
2023-06-05 10:01:14 -0500

    Finished Desktop Hero

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -5,14 +5,14 @@ const Hero = () => {
 		<section className="relative flex items-center h-screen z-30">
 			<div>
 				<div
-					className="bg-fixed bg-contain md:bg-cover bg-center absolute top-0 left-0 w-full h-full"
+					className="bg-fixed bg-contain bg-center absolute top-0 left-0 w-full h-full md:bg-cover"
 					style={{ backgroundImage: `url("${heroImage}")` }}
 				>
 					<div className="bg-black bg-opacity-60 h-screen flex flex-col justify-center items-center">
-						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] drop-shadow">
+						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] drop-shadow md:text-8xl">
 							Mulberry <span className="text-green-500">Tree</span>
 						</h1>
-						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] edrop-shadow pt-12">
+						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] edrop-shadow pt-12 md:text-8xl">
 							Saloon
 						</h1>
 					</div>
