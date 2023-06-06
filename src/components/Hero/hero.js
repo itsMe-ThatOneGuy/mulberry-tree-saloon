a4cf9c5aa2e14cea443c1c73e975ef61bfc8afb9
Matthew Smart <matthewsmart13@gmail.com>
2023-06-05 20:59:43 -0500

    Added query for larger displays

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -9,10 +9,10 @@ const Hero = () => {
 					style={{ backgroundImage: `url("${heroImage}")` }}
 				>
 					<div className="bg-black bg-opacity-60 h-screen flex flex-col justify-center items-center">
-						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] drop-shadow md:text-8xl">
+						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] drop-shadow md:text-5xl lg:text-8xl">
 							Mulberry <span className="text-green-500">Tree</span>
 						</h1>
-						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] edrop-shadow pt-12 md:text-8xl">
+						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] edrop-shadow pt-12 md:text-5xl lg:text-8xl">
 							Saloon
 						</h1>
 					</div>
