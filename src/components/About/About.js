410f8cf1f20fc09a4c9b048978a02f271184aed3
Matthew Smart <matthewsmart13@gmail.com>
2023-06-05 21:15:11 -0500

    Changed support for large screens

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,12 +3,12 @@ const About = () => {
 		<section id="about" className="relative w-full h-full scroll-m-12">
 			<div className="my-5">
 				<div className="bg-[#222] py-3">
-					<div className="flex flex-col items-center py-10 px-10 md:mx-80">
-						<h2 className="pb-10 font-medium text-green-500 text-2xl text-center uppercase tracking-widest md:text-5xl">
+					<div className="flex flex-col items-center py-10 px-10 lg:mx-80">
+						<h2 className="pb-10 font-medium text-green-500 text-2xl text-center uppercase tracking-widest lg:text-5xl">
 							- Our Story -
 						</h2>
 
-						<div className="text-white text-center md:text-2xl font-normal">
+						<div className="text-white text-center lg:text-2xl font-normal">
 							<p className="pb-3">
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -8,12 +8,12 @@ const Info = () => {
 					<h2 className="pb-10 mt-7 font-medium text-green-500 text-2xl text-center uppercase tracking-widest lg:text-5xl">
 						- Information -
 					</h2>
-					<div className="w-full md:grid grid-cols-2 text-white md:px-56 md:text-lg">
+					<div className="w-full lg:grid grid-cols-2 text-white lg:px-56 lg:text-lg">
 						<div className="flex flex-col items-center md:justify-center">
 							{/* Google maps image*/}
 							<div className="w-2/3">
 								<iframe
-									className="w-full h-72 md:h-[30rem]"
+									className="w-full h-72 lg:h-[30rem]"
 									title="test"
 									src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6373.141844684716!2d-93.63045578181291!3d36.99617396565907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87cf4eda7f665649%3A0xbeb42b2f2611bf8e!2s610%20State%20Hwy%20265%2C%20Marionville%2C%20MO%2065705!5e0!3m2!1sen!2sus!4v1683601716692!5m2!1sen!2sus"
 									loading="lazy"
@@ -22,9 +22,9 @@ const Info = () => {
 							</div>
 						</div>
 
-						<div className="md:flex justify-center md:w-full">
+						<div className="lg:flex justify-center lg:w-full">
 							{/* contact info */}
-							<div className="py-7 font-semibold text-lg md:text-xl">
+							<div className="py-7 font-semibold text-lg lg:text-xl">
 								<div className="flex flex-col items-center py-3">
 									<div className="flex pb-3">
 										<MapPin className="text-green-500" size={20} />
@@ -119,7 +119,7 @@ const Info = () => {
 							</div>
 						</div>
 					</div>
-					<div className="text-white text-center font-medium text-xl py-3 md:text-2xl">
+					<div className="text-white text-center font-medium text-xl py-3 lg:text-2xl">
 						<p>Visit our Facebook page for all upcoming Events!</p>
 					</div>
 					<div className="pb-3">
