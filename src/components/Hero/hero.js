4e35488cc41baeadff1fa29a4d9e4137f04f86ba
Matthew Smart <matthewsmart13@gmail.com>
2023-06-06 09:54:55 -0500

    Small styling changes

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,25 +1,25 @@
 import heroImage from '../../images/heroImage.jpg';
 
 const Hero = () => {
-	return (
-		<section className="relative flex items-center h-screen z-30">
-			<div>
-				<div
-					className="bg-fixed bg-contain bg-center absolute top-0 left-0 w-full h-full md:bg-cover"
-					style={{ backgroundImage: `url("${heroImage}")` }}
-				>
-					<div className="bg-black bg-opacity-60 h-screen flex flex-col justify-center items-center">
-						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] drop-shadow md:text-5xl lg:text-8xl">
-							Mulberry <span className="text-green-500">Tree</span>
-						</h1>
-						<h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] edrop-shadow pt-12 md:text-5xl lg:text-8xl">
-							Saloon
-						</h1>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+    return (
+        <section className="relative flex items-center h-screen z-30">
+            <div>
+                <div
+                    className="bg-fixed bg-contain bg-center absolute top-0 left-0 w-full h-full md:bg-cover"
+                    style={{ backgroundImage: `url("${heroImage}")` }}
+                >
+                    <div className="bg-black bg-opacity-70 h-screen flex flex-col justify-center items-center">
+                        <h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] drop-shadow md:text-5xl lg:text-8xl">
+                            Mulberry <span className="text-green-500">Tree</span>
+                        </h1>
+                        <h1 className="text-white text-3xl font-bold uppercase tracking-[.2em] edrop-shadow pt-12 md:text-5xl lg:text-8xl">
+                            Saloon
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Hero;
