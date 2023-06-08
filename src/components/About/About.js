99c32c8ffbbdfb50234817f04bbb1b7241804719
Matthew Smart <matthewsmart13@gmail.com>
2023-06-08 12:52:33 -0500

    Added animation to h2 and div

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,28 @@
+import { motion } from 'framer-motion';
+
 const About = () => {
 	return (
 		<section id="about" className="relative w-full h-full scroll-m-12">
 			<div className="my-5">
 				<div className="bg-[#222] py-3">
 					<div className="flex flex-col items-center py-10 px-10 lg:mx-80">
-						<h2 className="pb-10 font-medium text-green-500 text-2xl text-center uppercase tracking-widest lg:text-3xl">
+						<motion.h2
+							initial={{ opacity: 0 }}
+							whileInView={{ opacity: 1 }}
+							transition={{ duration: 1, delay: 0.5 }}
+							viewport={{ once: true }}
+							className="pb-10 font-medium text-green-500 text-2xl text-center uppercase tracking-widest lg:text-3xl"
+						>
 							- Our Story -
-						</h2>
+						</motion.h2>
 
-						<div className="text-white text-center lg:text-xl font-normal">
+						<motion.div
+							initial={{ opacity: 0 }}
+							whileInView={{ opacity: 1 }}
+							transition={{ duration: 1, delay: 0.75 }}
+							viewport={{ once: true }}
+							className="text-white text-center lg:text-xl font-normal"
+						>
 							<p className="pb-3">
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -36,7 +50,7 @@ const About = () => {
 								nulla pariatur. Excepteur sint occaecat cupidatat non proident,
 								sunt in culpa qui officia deserunt mollit anim id est laborum.
 							</p>
-						</div>
+						</motion.div>
 					</div>
 				</div>
 			</div>
