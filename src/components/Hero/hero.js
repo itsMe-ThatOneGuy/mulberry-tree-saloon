18281c583577c86a31db2d9b6df4878e1359b453
Matthew Smart <matthewsmart13@gmail.com>
2025-01-23 01:11:46 -0600

    Added dynamic background for desktop / mobile

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,13 +1,30 @@
-import heroImage from '../../images/heroImage.jpg';
+import React, { useState, useEffect } from 'react';
+import outsideMobile from '../../images/outside-mobile.jpg';
+import outside from '../../images/outside.jpg';
 import { motion } from 'framer-motion';
 
 const Hero = () => {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+	useEffect(() => {
+		const handleResize = () => setWindowWidth(window.innerWidth);
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	const getBackgroundImage = () => {
+		if (windowWidth < 768) {
+			return `url("${outsideMobile}")`;
+		} else {
+			return `url("${outside}")`;
+		}
+	};
+
 	return (
 		<section className="relative flex items-center h-screen z-30">
 			<div>
 				<div
 					className="bg-fixed bg-contain bg-center absolute top-0 left-0 w-full h-full md:bg-cover"
-					style={{ backgroundImage: `url("${heroImage}")` }}
 				>
 					<div className="bg-black bg-opacity-70 h-screen flex flex-col justify-center items-center">
 						<motion.div
@@ -24,6 +41,7 @@ const Hero = () => {
 							</h1>
 						</motion.div>
 					</div>
+				style={{ backgroundImage: getBackgroundImage() }}
 				</div>
 			</div>
 		</section>
