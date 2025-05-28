97d7479bbf67471c45faa115d1e7a61b9622723a
Matthew Smart <matthewsmart13@gmail.com>
2025-05-28 14:58:05 -0500

    Updated about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,7 +13,7 @@ const About = () => {
 							viewport={{ once: true }}
 							className="font-liquidism font-bold pb-10 mt-7 text-[#47A025] text-2xl text-center uppercase tracking-widest lg:text-3xl"
 						>
-							- Our Story -
+							- Dare to Enter -
 						</motion.h2>
 
 						<motion.div
@@ -24,31 +24,34 @@ const About = () => {
 							className="text-white text-center lg:text-xl font-normal"
 						>
 							<p className="pb-3">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-								enim ad minim veniam, quis nostrud exercitation ullamco laboris
-								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-								in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-								nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-								sunt in culpa qui officia deserunt mollit anim id est laborum.
+								Welcome to The Mulberry Tree Saloon, where the undead come alive
+								and the drinks never die. Nestled in the heart of Marionville,
+								Missouri, we’re not your average small-town watering hole—we're
+								a full-on zombie-themed experience where every night is a
+								thriller.
 							</p>
 							<p className="pb-3">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-								enim ad minim veniam, quis nostrud exercitation ullamco laboris
-								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-								in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-								nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-								sunt in culpa qui officia deserunt mollit anim id est laborum.
+								Whether you're dropping in for a cold one after work or looking
+								for a wild night of live music, karaoke, or billards with some
+								friends, there's always something happening at the Tree. Our
+								regulars are legends, our newcomers feel like family, and the
+								vibe? One part haunted hideout, one part hometown hangout.
 							</p>
 							<p className="pb-3">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-								enim ad minim veniam, quis nostrud exercitation ullamco laboris
-								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-								in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-								nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-								sunt in culpa qui officia deserunt mollit anim id est laborum.
+								And if you're feeling brave—or just plain hungry—step into The
+								City Morgue, our newly opened full kitchen serving up a
+								ghoulishly good menu of horror-themed eats and killer food
+								challenges. From brainy bites to monster-sized meals, it's a
+								dining experience to die for.
+							</p>
+							<p className="pb-8">
+								The Mulberry Tree Saloon isn’t just a bar—it’s a twisted slice
+								of small-town fun where zombies, locals, and legends collide. So
+								come thirsty, come hungry, and come ready for a good time.
+							</p>
+							<p className="font-liquidism2 text-3xl/10">
+								You don't have to be dead to enjoy it... <br />
+								but it helps.
 							</p>
 						</motion.div>
 					</div>
