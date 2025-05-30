69986c4a16c0bbe99e6279630f03e16ed97dc8aa
Matthew Smart <matthewsmart13@gmail.com>
2025-05-30 11:20:34 -0500

    style update

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,19 +1,20 @@
 import { Facebook, Instagram } from 'react-feather';
+
 const Footer = () => {
 	return (
 		<footer>
-			<div className="relative bg-[#222] w-full flex justify-between py-3 text-[#F0F7EE] items-center font-medium md:text-lg">
-				<div className="ml-7 text-lg">
+			<div className="relative w-full flex flex-col lg:flex-row justify-between items-center px-6 py-4 text-[#F0F7EE] font-medium md:text-lg bg-gradient-to-r from-black via-[#1a001f] to-black shadow-lg shadow-[#3b0764]/30 border-t border-[#3b0764]/50">
+				<div className="mb-2 lg:mb-0 lg:ml-0 text-lg">
 					<a
-						className="text-[#F0F7EE] hover:text-[#47A025] transition-all ease-in duration-300"
+						className="font-liquidism2 text-[#F0F7EE] hover:text-[#47A025] transition-all ease-in-out duration-300"
 						href="#top"
 					>
 						M<span className="font-liquidism text-[#47A025]">T</span>S
 					</a>
 				</div>
 
-				<div className="flex flex-col items-center">
-					<div className="flex flex-col lg:flex-row items-center">
+				<div className="text-center">
+					<div className="flex flex-col lg:flex-row items-center gap-1">
 						<div className="flex">
 							<p className="pr-1">© {new Date().getFullYear()}</p>
 							<p className="pr-1 hidden lg:inline"> &middot; </p>
@@ -26,26 +27,25 @@ const Footer = () => {
 							Saloon
 						</p>
 					</div>
-					<p>All rights reserved</p>
+					<p className="text-sm opacity-70">All rights reserved</p>
 				</div>
-				<div className="flex">
+
+				<div className="flex mt-2 lg:mt-0 space-x-4">
 					<a
 						href="https://www.facebook.com/profile.php?id=100057060955346"
 						target="_blank"
 						rel="noreferrer"
+						className="hover:text-blue-600 transition-colors duration-300"
 					>
-						<button className=" lg:mr-3 hover:text-blue-600">
-							<Facebook size={30} />
-						</button>
+						<Facebook size={26} />
 					</a>
 					<a
-						href="https://www.facebook.com/profile.php?id=100057060955346"
+						href="https://www.instagram.com/mulberrytreesaloon/"
 						target="_blank"
 						rel="noreferrer"
+						className="hover:text-[#DD2A7B] transition-colors duration-300"
 					>
-						<button className="mr-7 hover:text-[#DD2A7B]">
-							<Instagram size={30} />
-						</button>
+						<Instagram size={26} />
 					</a>
 				</div>
 			</div>
