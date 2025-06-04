3db1acd0f74cf8d3e838f06bfad6b1d295f974f3
Matthew Smart <matthewsmart13@gmail.com>
2025-06-04 14:57:31 -0500

    updated menu page

---
+++ src/pages/Menu.js
@@ -0,0 +1,11 @@
+import Menu from '../components/Menu/menu';
+import ScrollToTopBtn from '../components/ScrollToTopBtn/scrolltotopbtn';
+
+const MenuPage = () => (
+	<>
+		<Menu />
+		<ScrollToTopBtn />
+	</>
+);
+
+export default MenuPage;
