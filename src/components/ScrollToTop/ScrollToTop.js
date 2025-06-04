d259e8b300aae72f63d3d9cbca76219570603e89
Matthew Smart <matthewsmart13@gmail.com>
2025-06-04 11:28:29 -0500

    scroll to top higher order func

---
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ScrollToTop = () => {
+	const location = useLocation();
+
+	useEffect(() => {
+		if (location.pathname === '/menu' || location.pathname === '/') {
+			window.scrollTo({ top: 0, behavior: 'smooth' });
+		}
+	}, [location.key]);
+
+	return null;
+};
+
+export default ScrollToTop;
