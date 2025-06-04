d79fcd79eb71b5aa62e117ecc2ea2402364c8984
Matthew Smart <matthewsmart13@gmail.com>
2025-06-04 11:22:22 -0500

    created the homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero/hero';
+import About from '../components/About/About';
+import Gallery from '../components/Gallery/gallery';
+import Info from '../components/Info/info';
+
+const Home = () => (
+	<>
+		<Hero />
+		<About />
+		<Gallery />
+		<Info />
+	</>
+);
+
+export default Home;
